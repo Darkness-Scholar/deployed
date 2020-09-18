f9f311fb81481e0bac583cade4b72125454cc654
Tung Hoang <tungxm113@gmail.com>
2020-09-18 09:58:54 +0700

    Updates

---
+++ precache-manifest.67e375ad21485db1ef804f3a6e0d9120.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c70c37756dd528b5c6df815df4e5c025",
+    "url": "/deployed/index.html"
+  },
+  {
+    "revision": "7ddda8adc40bbc412115",
+    "url": "/deployed/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "340aac5933c756125f93",
+    "url": "/deployed/static/js/2.a0c31166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deployed/static/js/2.a0c31166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ddda8adc40bbc412115",
+    "url": "/deployed/static/js/main.78835a5b.chunk.js"
+  },
+  {
+    "revision": "043a7d92322274ed8863",
+    "url": "/deployed/static/js/runtime-main.d1e299d0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deployed/static/media/logo.5d5d9eef.svg"
+  }
+]);
